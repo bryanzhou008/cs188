c358c93b48922fc0c0c5628ef8644e59271d3acd
Yunqiu Han <yunqiu21@g.ucla.edu>
2022-11-06 19:03:38 -0800

    Revert "change model to args.model_name_or_path"
    This reverts commit 5d7ce82f55e847deda70c21b4932900f339fb3a9.

--- trainers/train.py
+++ trainers/train.py
@@ -663,15 +663,13 @@ def main():
     # for essential args.
 
     # TODO: Huggingface configs.
-    config = AutoConfig.from_pretrained(args.model_name_or_path)    # for bert
-    # config = AutoConfig.from_pretrained("textattack/bert-base-uncased-yelp-polarity")    # for bert
+    config = AutoConfig.from_pretrained("textattack/bert-base-uncased-yelp-polarity")    # for bert
     # config = AutoConfig.from_pretrained("yangheng/deberta-v3-base-absa-v1.1")   # for deberta
 
 
 
     # TODO: Tokenizer.
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)  # for bert
-    # tokenizer = AutoTokenizer.from_pretrained("textattack/bert-base-uncased-yelp-polarity")  # for bert
+    tokenizer = AutoTokenizer.from_pretrained("textattack/bert-base-uncased-yelp-polarity")  # for bert
     # tokenizer = AutoTokenizer.from_pretrained("yangheng/deberta-v3-base-absa-v1.1") # for deberta
 
 
@@ -685,8 +683,7 @@ def main():
             config=config,
         )
     else:
-        model = AutoModelForSequenceClassification.from_pretrained(args.model_name_or_path)    # for bert
-        # model = AutoModelForSequenceClassification.from_pretrained("textattack/bert-base-uncased-yelp-polarity")    # for bert
+        model = AutoModelForSequenceClassification.from_pretrained("textattack/bert-base-uncased-yelp-polarity")    # for bert
         # model = AutoModelForSequenceClassification.from_pretrained("yangheng/deberta-v3-base-absa-v1.1") # for deberta
 
     # End of TODO.
