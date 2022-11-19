13ee2072a3291c847bf725ce9bc8c827b3cea393
Yunqiu Han <yunqiu21@g.ucla.edu>
2022-11-19 15:56:28 -0800

    change script

--- scripts/train_com2sense.sh
+++ scripts/train_com2sense.sh
@@ -1,30 +1,30 @@
 TASK_NAME="com2sense"
 DATA_DIR="datasets/com2sense"
-MODEL_TYPE="bert-large-cased-whole-word-masking"
+# MODEL_TYPE="bert-large-cased-whole-word-masking" 
+MODEL_TYPE="mediabiasgroup/DA-RoBERTa-BABE" 
+SUBDIR="mediabias"
 
 
 python3 -m trainers.train \
   --model_name_or_path ${MODEL_TYPE} \
-  --do_not_load_optimizer \
   --do_train \
   --do_eval \
   --evaluate_during_training \
   --per_gpu_train_batch_size 16 \
   --per_gpu_eval_batch_size 1 \
-  --gradient_accumulation_steps 4 \
-  --learning_rate 5e-6 \
+  --gradient_accumulation_steps 8 \
+  --learning_rate 1e-5 \
   --max_steps 2000 \
   --max_seq_length 128 \
-  --output_dir "${TASK_NAME}/ckpts" \
+  --output_dir "${TASK_NAME}/${SUBDIR}" \
   --task_name "${TASK_NAME}" \
   --data_dir "${DATA_DIR}" \
   --overwrite_output_dir \
   --save_steps 20 \
-  --logging_steps 10 \
+  --logging_steps 20 \
   --warmup_steps 100 \
   --eval_split "dev" \
   --score_average_method "binary" \
-  --iters_to_eval 20 40 \
+  --eval_all_checkpoints \
   --overwrite_output_dir \
   --eval_split "dev" \
-  # --max_eval_steps 1000 \
