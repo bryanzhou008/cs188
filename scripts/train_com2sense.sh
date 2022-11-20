cd257b1a310919698b8f34e34d1be1b5f6a76e84
Yunqiu Han <yunqiu21@g.ucla.edu>
2022-11-19 19:26:28 -0800

    change script

--- scripts/train_com2sense.sh
+++ scripts/train_com2sense.sh
@@ -1,10 +1,6 @@
 TASK_NAME="com2sense"
 DATA_DIR="datasets/com2sense"
 
-# https://arxiv.org/pdf/1810.04805.pdf
-# MODEL_TYPE="bert-large-cased-whole-word-masking" 
-# SUBDIR="wholeword"
-
 # https://arxiv.org/pdf/2209.14557.pdf
 MODEL_TYPE="mediabiasgroup/DA-RoBERTa-BABE" 
 SUBDIR="mediabias"  
@@ -17,17 +13,17 @@ python3 -m trainers.train \
   --evaluate_during_training \
   --per_gpu_train_batch_size 16 \
   --per_gpu_eval_batch_size 1 \
-  --gradient_accumulation_steps 4 \
-  --num_train_epochs 4 \
+  --gradient_accumulation_steps 8 \
   --learning_rate 5e-5 \
+  --max_steps 2000 \
   --max_seq_length 128 \
   --output_dir "${TASK_NAME}/${SUBDIR}" \
   --task_name "${TASK_NAME}" \
   --data_dir "${DATA_DIR}" \
   --overwrite_output_dir \
-  --save_steps 200 \
+  --save_steps 20 \
   --logging_steps 20 \
+  --warmup_steps 100 \
   --eval_split "dev" \
   --score_average_method "binary" \
-  --eval_all_checkpoints \
-
+  --eval_all_checkpoints
