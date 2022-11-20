9312f700fe5ba532c1fc9143f376c1867011f629
Yunqiu Han <yunqiu21@g.ucla.edu>
2022-11-19 18:14:38 -0800

    change script

--- scripts/train_com2sense.sh
+++ scripts/train_com2sense.sh
@@ -1,8 +1,13 @@
 TASK_NAME="com2sense"
 DATA_DIR="datasets/com2sense"
-# MODEL_TYPE="bert-large-cased-whole-word-masking" 
-MODEL_TYPE="mediabiasgroup/DA-RoBERTa-BABE" 
-SUBDIR="mediabias"
+
+# https://arxiv.org/pdf/1810.04805.pdf
+MODEL_TYPE="bert-large-cased-whole-word-masking" 
+SUBDIR="wholeword"
+
+# https://arxiv.org/pdf/2209.14557.pdf
+# MODEL_TYPE="mediabiasgroup/DA-RoBERTa-BABE" 
+# SUBDIR="mediabias"  
 
 
 python3 -m trainers.train \
@@ -12,19 +17,18 @@ python3 -m trainers.train \
   --evaluate_during_training \
   --per_gpu_train_batch_size 16 \
   --per_gpu_eval_batch_size 1 \
-  --gradient_accumulation_steps 8 \
-  --learning_rate 1e-5 \
-  --max_steps 2000 \
+  --gradient_accumulation_steps 4 \
+  --num_train_epochs 4 \
+  --learning_rate 5e-5 \
   --max_seq_length 128 \
+  --weight_decay 0.01 \
   --output_dir "${TASK_NAME}/${SUBDIR}" \
   --task_name "${TASK_NAME}" \
   --data_dir "${DATA_DIR}" \
   --overwrite_output_dir \
-  --save_steps 20 \
+  --save_steps 200 \
   --logging_steps 20 \
-  --warmup_steps 100 \
   --eval_split "dev" \
   --score_average_method "binary" \
   --eval_all_checkpoints \
-  --overwrite_output_dir \
-  --eval_split "dev" \
+
--- trainers/train.py
+++ trainers/train.py
@@ -667,7 +667,8 @@ def main():
             config=config,
         )
     else:
-        model = AutoModelForSequenceClassification.from_pretrained(
+        model = AutoModelForMaskedLM.from_pretrained(
+        # model = AutoModelForSequenceClassification.from_pretrained( 
             args.model_name_or_path,
             from_tf=bool(".ckpt" in args.model_name_or_path),
             config=config,
