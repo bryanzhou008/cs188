d9e2506e65d0f7ed1863de068ab5a5fe48e93cd4
Yunqiu Han <yunqiu21@g.ucla.edu>
2022-11-20 00:18:35 -0800

    roberta-base

--- scripts/train_com2sense.sh
+++ scripts/train_com2sense.sh
@@ -1,10 +1,9 @@
 TASK_NAME="com2sense"
 DATA_DIR="datasets/com2sense"
 
-# https://arxiv.org/pdf/2209.14557.pdf
-MODEL_TYPE="mediabiasgroup/DA-RoBERTa-BABE" 
-SUBDIR="mediabias"  
-
+# https://arxiv.org/pdf/1907.11692.pdf
+MODEL_TYPE="roberta-base" 
+SUBDIR="robertabase"  
 
 python3 -m trainers.train \
   --model_name_or_path ${MODEL_TYPE} \
@@ -13,17 +12,18 @@ python3 -m trainers.train \
   --evaluate_during_training \
   --per_gpu_train_batch_size 16 \
   --per_gpu_eval_batch_size 1 \
-  --gradient_accumulation_steps 8 \
-  --learning_rate 5e-5 \
+  --gradient_accumulation_steps 4 \
+  --learning_rate 1e-5 \
   --max_steps 2000 \
   --max_seq_length 128 \
+  --weight_decay 0.01 \
   --output_dir "${TASK_NAME}/${SUBDIR}" \
   --task_name "${TASK_NAME}" \
   --data_dir "${DATA_DIR}" \
+  --overwrite_output_dir \
   --save_steps 200 \
   --logging_steps 20 \
-  --warmup_steps 100 \
+  --warmup_steps 120 \
+  --eval_split "dev" \
   --score_average_method "binary" \
   --eval_all_checkpoints
-  --eval_split "dev" \
-  --overwrite_output_dir \
