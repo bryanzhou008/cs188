f31de519ad63ac12d12a6878dffc7b038402e399
Yunqiu Han <yunqiu21@g.ucla.edu>
2022-11-24 22:06:45 -0800

    test

---
+++ scripts/test_com2sense.sh
@@ -0,0 +1,16 @@
+TASK_NAME="com2sense"
+DATA_DIR="datasets/com2sense"
+SUBDIR="semevalckpt" 
+
+
+python3 -m trainers.train \
+  --model_name_or_path "${TASK_NAME}/${SUBDIR}/checkpoint-1000" \
+  --do_eval \
+  --iters_to_eval checkpoint-2000 \
+  --per_gpu_eval_batch_size 1 \
+  --max_seq_length 128 \
+  --output_dir "${TASK_NAME}/${SUBDIR}" \
+  --task_name "${TASK_NAME}" \
+  --data_dir "${DATA_DIR}" \
+  --eval_split "test" \
+  --do_not_load_optimizer \
