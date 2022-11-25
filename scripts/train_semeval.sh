624c246d88d63813c7f453349809fc647fd817d9
Yunqiu Han <yunqiu21@g.ucla.edu>
2022-11-25 00:08:16 -0800

    increase lr for train semeval

--- scripts/train_semeval.sh
+++ scripts/train_semeval.sh
@@ -10,16 +10,16 @@ python3 -m trainers.train \
   --per_gpu_train_batch_size 6 \
   --per_gpu_eval_batch_size 1 \
   --gradient_accumulation_steps 8 \
-  --max_steps 100 \
+  --max_steps 200 \
   --max_seq_length 128 \
-  --learning_rate 2e-5 \
+  --learning_rate 4e-5 \
   --adam_epsilon 1e-6 \
   --weight_decay 0.01 \
   --output_dir "${TASK_NAME}/${SUBDIR}" \
   --task_name "${TASK_NAME}" \
   --data_dir "${DATA_DIR}" \
   --overwrite_output_dir \
-  --save_steps 20 \
+  --save_steps 50 \
   --logging_steps 5 \
   --eval_split "dev" \
   --score_average_method "binary" \
