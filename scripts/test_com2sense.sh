7486ed6b1b50729c71d17ae201ac2360f3b32dc9
Yunqiu Han <yunqiu21@g.ucla.edu>
2022-11-24 23:43:59 -0800

    use best for test

--- scripts/test_com2sense.sh
+++ scripts/test_com2sense.sh
@@ -2,11 +2,10 @@ TASK_NAME="com2sense"
 DATA_DIR="datasets/com2sense"
 SUBDIR="semevalckpt" 
 
-
 python3 -m trainers.train \
-  --model_name_or_path "outputs/${TASK_NAME}/${SUBDIR}/checkpoint-1000" \
+  --model_name_or_path "outputs/${TASK_NAME}/${SUBDIR}/checkpoint-best" \
   --do_eval \
-  --iters_to_eval 1000 \
+  --iters_to_eval "best" \
   --per_gpu_eval_batch_size 1 \
   --max_seq_length 128 \
   --output_dir "${TASK_NAME}/${SUBDIR}" \
