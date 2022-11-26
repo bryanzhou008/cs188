7abdcaa4ef45c705f0d28cf3545e4890adae0d8b
Yunqiu Han <yunqiu21@g.ucla.edu>
2022-11-25 17:31:10 -0800

    add to script

--- scripts/all_scripts.sh
+++ scripts/all_scripts.sh
@@ -232,6 +232,34 @@ python3 -m trainers.train \
   --score_average_method "binary" \
   --eval_all_checkpoints
 
+# Best
+TASK_NAME="semeval"
+DATA_DIR="datasets/semeval_2020_task4"
+MODEL_TYPE="microsoft/deberta-v3-large" 
+SUBDIR="debertav3large"
+python3 -m trainers.train \
+  --model_name_or_path ${MODEL_TYPE} \
+  --do_train \
+  --do_eval \
+  --evaluate_during_training \
+  --per_gpu_train_batch_size 6 \
+  --per_gpu_eval_batch_size 1 \
+  --gradient_accumulation_steps 8 \
+  --max_steps 200 \
+  --max_seq_length 128 \
+  --learning_rate 4e-5 \
+  --adam_epsilon 1e-6 \
+  --weight_decay 0.01 \
+  --output_dir "${TASK_NAME}/${SUBDIR}" \
+  --task_name "${TASK_NAME}" \
+  --data_dir "${DATA_DIR}" \
+  --overwrite_output_dir \
+  --save_steps 50 \
+  --logging_steps 5 \
+  --eval_split "dev" \
+  --score_average_method "binary" \
+  --eval_all_checkpoints
+
 TASK_NAME="com2sense"
 DATA_DIR="datasets/com2sense"
 MODEL_TYPE="outputs/semeval/debertav3large/checkpoint-best" 
